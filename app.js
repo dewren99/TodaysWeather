3b45ce27406557f8a7ae8de18fdc484c2fb53972
Deniz Evrendilek <devrendilek@gmail.com>
2019-08-15 02:32:02 -0700

    mapbox API + geolocation func abstraction

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const request = require('request')
+const geocode = require('./utilities/geocode.js')
 
-const url = 'https://api.darksky.net/forecast/2fcd47c18a1f736eefdf8c451e925067/37.8267,-122.4233'
+// const url = 'https://api.darksky.net/forecast/2fcd47c18a1f736eefdf8c451e925067/37.8267,-122.4233'
 
-request({url: url, json: true}, (error, response)=>{
-    //console.log(response.body.currently)
-    console.log('Currently ' + response.body.currently.temperature + ' Degree and ' + response.body.currently.precipProbability  + '% chance of rain')
+// // request({url: url, json: true}, (error, response)=>{
+// //     //console.log(response.body.currently)
+// //     console.log('Currently ' + response.body.currently.temperature + ' Degree and ' + response.body.currently.precipProbability  + '%' + ' chance of rain')
+// // })
+
+geocode('Burnaby', (error,data)=>{
+    console.log('Error', error)
+    console.log('Data', data)
 })
