ad5534181233867c3d4ab3e42a44b8ef32c39d68
Deniz Evrendilek <devrendilek@gmail.com>
2019-08-20 23:44:24 -0700

    heroku deployment

--- server/source/app.js
+++ server/source/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utilities/geocode')
 const forecast = require('./utilities/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, '../templates/views'))
@@ -87,8 +88,8 @@ app.get('*',(req,res)=>{
     })
 })
 
-app.listen(3000, () =>{
-    console.log('server at 3000')
+app.listen(port, () =>{
+    console.log('server at ' + port)
 })
 
 
